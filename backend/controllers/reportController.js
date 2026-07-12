e8318ff4b28242a03654078d7281975f3ff833e4
Akashi Verma <akashiverma@Akashis-MacBook-Air.local>
2026-07-12 13:08:12 +0530

    improved controller

--- backend/controllers/reportController.js
+++ backend/controllers/reportController.js
@@ -4,34 +4,35 @@ exports.getAchievementReport = async (req, res) => {
   try {
 
     const [report] = await db.query(`
-      SELECT
-        s.Name AS SalesPerson,
-        p.Name AS Product,
-        MAX(t.TargetQty) AS TargetQty,
-        SUM(IFNULL(a.AchQty,0)) AS AchievementQty,
-        MAX(t.TargetValue) AS TargetValue,
-        SUM(IFNULL(a.AchValue,0)) AS AchievementValue
+SELECT
+    s.Name AS SalesPerson,
+    p.Name AS Product,
+    SUM(t.TargetQty) AS TargetQty,
+    COALESCE(SUM(a.AchQty), 0) AS AchQty,
+    SUM(t.TargetValue) AS TargetValue,
+    COALESCE(SUM(a.AchValue), 0) AS AchValue
 
-      FROM tbl_Target t
+FROM tbl_Target t
 
-      JOIN tbl_SalesPerson s
-        ON t.SP_ID = s.SP_ID
+JOIN tbl_SalesPerson s
+ON t.SP_ID = s.SP_ID
 
-      JOIN tbl_Product p
-        ON t.ProductID = p.ProductID
+JOIN tbl_Product p
+ON t.ProductID = p.ProductID
 
-      LEFT JOIN tbl_Achievement a
-        ON t.SP_ID = a.SP_ID
-        AND t.ProductID = a.ProductID
-        AND a.IsActive = 1
+LEFT JOIN tbl_Achievement a
+ON t.SP_ID = a.SP_ID
+AND t.ProductID = a.ProductID
+AND a.IsActive = 1
 
-      WHERE t.IsActive = 1
+WHERE t.IsActive = 1
 
-      GROUP BY
-        s.Name,
-        p.Name
-    `);
-
+GROUP BY
+t.SP_ID,
+t.ProductID,
+s.Name,
+p.Name
+`);
     return res.status(200).json(report);
 
   } catch (error) {
